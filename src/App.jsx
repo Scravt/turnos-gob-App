d62a9fd29996414a4a75a4b4fb807e111dc5c5a2
Gonzalo <34680110ax@gmail.com>
2024-11-21 20:32:05 -0300

    react-big-calendar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
 import { Home } from './pages/home/Home';
+import Turn from './pages/turn/Turn';
 
 
 function App() {
@@ -13,6 +14,10 @@ function App() {
         <Route path="/" element={<Home/> } />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/turn" element={<Turn />} />
+        <Route path="*" element={
+          <div>404</div>
+        } />
        
       </Routes>
   </div>
--- src/pages/turn/Turn.jsx
+++ src/pages/turn/Turn.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Calendar, dayjsLocalizer } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import dayjs from 'dayjs'
 
 const Turn = () => {
+  const localizer = dayjsLocalizer(dayjs)
   return (
-    <div>Turn</div>
+    <div className='h-screen w-screen flex items-center justify-center'>
+      <div className='h-1/3 w-1/2'>
+        <Calendar
+          localizer={localizer}
+        />
+      </div>
+
+
+    </div>
   )
 }
 
